830f3f6761c75d8dc22872faf2751a888e6c35e2
ZJianbo <857272247@qq.com>
2024-07-31 15:27:54 +0800

    feat(http请求练习): 暂存

--- httpModule/07_http请求练习.js
+++ httpModule/07_http请求练习.js
@@ -2,6 +2,15 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
+  // 获取请求方法
+  let { method } = request;
+  // 获取请求路径
+  let { pathname } = new URL(request.url, 'http://127.0.0.1');
+  console.log(method);
+  console.log(pathname);
+  if (method === 'GET' && pathname === '/login') {
+    response.end('这是登录页面')
+  }
   response.end('practise');
 })
 
