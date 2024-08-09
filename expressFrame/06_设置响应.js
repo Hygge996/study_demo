b26f0b11c9445a2ceb082014201a53f9846848a0
ZJianbo <857272247@qq.com>
2024-08-09 17:09:14 +0800

    feat(express框架): 学习express api

--- expressFrame/06_设置响应.js
+++ expressFrame/06_设置响应.js
@@ -2,10 +2,20 @@ const express = require('express')
 
 const app = express();
 
-app.get((req, res) => {
-  res.end('test')
+app.get('/response',(req, res) => {
+  // res.statusCode = 404;
+  // res.statusMessage = 'nice';
+  // res.setHeader('xxx', 'yyy');
+  // res.write('hello express');
+  // res.end('response');
+
+  // express 响应
+  res.status(500);
+  res.set('aaa', 'bbb');
+  res.send('你好 express')
 })
 
 app.listen(3000, () => {
   console.log('启动！超级变换形态。。。');
+
 })
