1a312d872d7021f442faede4f02f2685a8fc0f24
ZHAOJianbo <857272247@qq.com>
2024-09-02 21:47:56 +0800

    feat(记账本): 账单新增

--- accounts/routes/index.js
+++ accounts/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync( __dirname + '/../data/db.json')
+const db = low(adapter)
+const shortid = require('shortid')
 
 /* 记账本列表 */
 router.get('/account', function(req, res, next) {
-  res.render('list');
+  //获取所有的账单信息
+  let accounts = db.get('accounts').value();
+  res.render('list', {accounts: accounts});
 });
 
 /* 添加记录 */
@@ -11,10 +19,13 @@ router.get('/account/create', function(req, res, next) {
   res.render('create');
 });
 
-router.post('/account', (req, res, next) => {
-  console.log(req.body);
-  
-  res.send('添加记录')
-})
+router.post('/account', (req, res) => {
+  //生成 id
+  let id = shortid.generate();
+  //写入文件
+  db.get('accounts').unshift({id:id, ...req.body}).write();
+  //成功提醒
+  res.render('success', {msg: '添加成功哦~~~', url: '/account'});
+});
 
 module.exports = router;
