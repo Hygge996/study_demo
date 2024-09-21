e9d8e26dd773baf25586f4fd9181d5c14f75b73b
ZHAOJianbo <857272247@qq.com>
2024-09-21 16:24:43 +0800

    feat(记账本):  修改错误语法

--- mongodb数据库/03_记账本/routes/index.js
+++ mongodb数据库/03_记账本/routes/index.js
@@ -22,13 +22,12 @@ router.get('/account', function(req, res, next) {
   //获取所有的账单信息
   // let accounts = db.get('accounts').value();
   //读取集合信息
-  AccountModel.find().sort({time: -1}).exec((err, data) => {
-    if(err){
-      res.status(500).send('读取失败~~~');
-      return;
-    }
+  AccountModel.find().sort({time: -1}).exec().then((data) => {
     //响应成功的提示
     res.render('list', {accounts: data, moment: moment});
+  }).catch((err) => {
+    res.status(500).send('读取失败~~~');
+    return;
   })
 });
 
@@ -44,13 +43,14 @@ router.post('/account', (req, res) => {
     ...req.body,
     //修改 time 属性的值
     time: moment(req.body.time).toDate()
-  }, (err, data) => {
-    if(err){
-      res.status(500).send('插入失败~~');
-      return
-    }
+  })
+  .then((data) => {
     //成功提醒
     res.render('success', {msg: '添加成功哦~~~', url: '/account'});
+  })
+  .catch ((err) => {
+    res.status(500).send('插入失败~~');
+    return
   })
 });
 
@@ -59,13 +59,12 @@ router.get('/account/:id', (req, res) => {
   //获取 params 的 id 参数
   let id = req.params.id;
   //删除
-  AccountModel.deleteOne({_id: id}, (err, data) => {
-    if(err) {
-      res.status(500).send('删除失败~');
-      return;
-    }
+  AccountModel.deleteOne({_id: id}).then((data) => {
     //提醒
     res.render('success', {msg: '删除成功~~~', url: '/account'});
+  }).catch((err) => {
+    res.status(500).send('删除失败~');
+    return;
   })
 });
 
