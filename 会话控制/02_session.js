c72c1abd34dc562cc60555116f2d588809e9bff5
ZJianbo <857272247@qq.com>
2024-09-22 17:50:15 +0800

    feat(会话控制): session的设置

--- 会话控制/02_session.js
+++ 会话控制/02_session.js
@@ -11,28 +11,46 @@ app.use(session({
   saveUninitialized: false, //是否为每次请求都设置一个cookie用来存储session的id
   resave: true, //是否在每次请求时重新保存session
   store: MongoStore.create({
-    mongoUrl: 'mongodb://127.0.0.1:27017/project' //数据库的连接配置
+    mongoUrl: 'mongodb://127.0.0.1:27017/bilibili' //数据库的连接配置
   }),
   cookie: {
     httpOnly: true, // 开启后前端无法通过 JS 操作
-    maxAge: 1000 * 300 // 这一条 是控制 sessionID 的过期时间的！！！
+    maxAge: 1000 * 30 // 这一条 是控制 sessionID 的过期时间的！！！
   },
 }))
-// 设置cookie
-app.get('/set-cookie', (req, res) => {
-  res.cookie('name', 'zhangsan')
-  res.cookie('email', '110@qq.com', { maxAge: 3 * 1000 });
+// 首页路由
+app.get('/', (req, res) => {
   res.send('home');
 })
-// 读取 cookie
-app.get('/get-cookie', (req, res) => {
-  console.log(req.cookies);
-  res.send(`Cookie的读取数-${req.cookies}`)
+
+// 登录
+app.get('/login', (req, res) => {
+  if (req.query.username === 'admin' && req.query.password === 'admin') {
+    req.session.username = 'admin'
+    res.send('登录成功！')
+  } else {
+    res.send('登录失败！')
+    return;
+  }
 })
-// 删除 cooklie
-app.get('/del-cookie', (req, res) => {
-  res.clearCookie('name', 'zhangsan')
-  res.send('清除 cookie');
+
+// session 的读取
+app.get('/cart', (req, res) => {
+  if (req.session.username) {
+    res.send('登录成功，欢迎光临')
+  } else {
+    res.send('登录失败')
+    return
+  }
 })
+
+//销毁 session
+app.get('/logout', (req, res) => {
+  //销毁session
+  // res.send('设置session');
+  req.session.destroy(() => {
+  res.send('成功退出');
+  });
+  });  
 
 app.listen(3000);
